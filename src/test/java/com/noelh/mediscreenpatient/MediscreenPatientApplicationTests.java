106aa5d3f8d6605e588a8b35d1225b99e561d8f9
Noel-H <noel.huberto@gmail.com>
2022-09-04 14:39:08 +0200

    Initial commit

---
+++ src/test/java/com/noelh/mediscreenpatient/MediscreenPatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noelh.mediscreenpatient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenPatientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
